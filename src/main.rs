3cafe1ce96c73886186e10e74814f3f005843ee2
Doug Aaser <doug@aaser.net>
2025-03-02 20:39:15 -0500

    Reformat

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-use std::{
-  io,
-  time::{Duration, Instant},
-};
+use std::io;
+use std::time::{Duration, Instant};
 
-use crossterm::{
-  event::{self, Event, KeyCode},
-  execute,
-  terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+use crossterm::event::{self, Event, KeyCode};
+use crossterm::execute;
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
 };
-use tui::{
-  backend::{Backend, CrosstermBackend},
-  layout::Rect,
-  widgets::Paragraph,
-  Frame, Terminal,
-};
+use tui::backend::{Backend, CrosstermBackend};
+use tui::layout::Rect;
+use tui::widgets::Paragraph;
+use tui::{Frame, Terminal};
 
 const C: f64 = 1.45;
 const Q: f64 = 0.5;
@@ -21,110 +17,112 @@ const G: f64 = 0.25;
 const P: f64 = 0.25;
 
 fn main() -> anyhow::Result<()> {
-  enable_raw_mode()?;
-  let mut stdout = io::stdout();
-  execute!(stdout, EnterAlternateScreen)?;
-  let backend = CrosstermBackend::new(stdout);
-  let mut terminal = Terminal::new(backend)?;
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
-  let size = terminal.size()?;
-  if size.width < 74 || size.height < 21 {
+    let size = terminal.size()?;
+    if size.width < 74 || size.height < 21 {
+        quit(&mut terminal)?;
+        println!("terminal too small, must be at least 74x21");
+        return Ok(());
+    }
+
+    let tick_rate = Duration::from_millis(20);
+    let eye = Eye::new(size);
+    let res = run(&mut terminal, eye, tick_rate);
+
     quit(&mut terminal)?;
-    println!("terminal too small, must be at least 74x21");
-    return Ok(());
-  }
 
-  let tick_rate = Duration::from_millis(20);
-  let eye = Eye::new(size);
-  let res = run(&mut terminal, eye, tick_rate);
-
-  quit(&mut terminal)?;
-
-  if let Err(err) = res {
-    println!("{err:?}");
-  }
-  Ok(())
+    if let Err(err) = res {
+        println!("{err:?}");
+    }
+    Ok(())
 }
 
 fn quit<B>(terminal: &mut Terminal<B>) -> anyhow::Result<()>
 where
-  B: Backend + io::Write,
+    B: Backend + io::Write,
 {
-  disable_raw_mode()?;
-  execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
-  terminal.show_cursor()?;
-  Ok(())
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+    terminal.show_cursor()?;
+    Ok(())
 }
 
 fn run<B: Backend>(
-  terminal: &mut Terminal<B>, mut eye: Eye, tick_rate: Duration,
+    terminal: &mut Terminal<B>,
+    mut eye: Eye,
+    tick_rate: Duration,
 ) -> anyhow::Result<()> {
-  let mut last_tick = Instant::now();
-  for m in 1.. {
-    terminal.draw(|f| ui(f, &eye))?;
-    let timeout = tick_rate
-      .checked_sub(last_tick.elapsed())
-      .unwrap_or_else(|| Duration::from_secs(0));
-    if crossterm::event::poll(timeout)? {
-      if let Event::Key(key) = event::read()? {
-        if let KeyCode::Char('q') = key.code {
-          return Ok(());
+    let mut last_tick = Instant::now();
+    for m in 1.. {
+        terminal.draw(|f| ui(f, &eye))?;
+        let timeout = tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+        if crossterm::event::poll(timeout)? {
+            if let Event::Key(key) = event::read()? {
+                if let KeyCode::Char('q') = key.code {
+                    return Ok(());
+                }
+            }
         }
-      }
+        if last_tick.elapsed() >= tick_rate {
+            eye.tick(m)?;
+            last_tick = Instant::now();
+        }
     }
-    if last_tick.elapsed() >= tick_rate {
-      eye.tick(m)?;
-      last_tick = Instant::now();
-    }
-  }
-  Ok(())
+    Ok(())
 }
 
 fn ui<B: Backend>(f: &mut Frame<B>, eye: &Eye) {
-  let label = Paragraph::new(eye.state.clone());
-  f.render_widget(label, f.size());
+    let label = Paragraph::new(eye.state.clone());
+    f.render_widget(label, f.size());
 }
 
 struct Eye {
-  state: String,
-  height: u16,
-  width: u16,
-  v: Box<dyn Fn(f64, f64, f64) -> usize>,
+    state: String,
+    height: u16,
+    width: u16,
+    v: Box<dyn Fn(f64, f64, f64) -> usize>,
 }
 
 impl Eye {
-  fn new(size: Rect) -> Self {
-    let v = move |x: f64, t: f64, s: f64| {
-      let a = (x - C).abs();
-      let j = (t - Q).abs();
-      if j > P || a > 2.0 * G {
-        0
-      } else {
-        (s * -(2.0 * P * G - P * a - G * j) + s) as usize
-      }
-    };
-    Self {
-      v: Box::new(v),
-      state: String::default(),
-      height: size.height,
-      width: size.width,
+    fn new(size: Rect) -> Self {
+        let v = move |x: f64, t: f64, s: f64| {
+            let a = (x - C).abs();
+            let j = (t - Q).abs();
+            if j > P || a > 2.0 * G {
+                0
+            } else {
+                (s * -(2.0 * P * G - P * a - G * j) + s) as usize
+            }
+        };
+        Self {
+            v: Box::new(v),
+            state: String::default(),
+            height: size.height,
+            width: size.width,
+        }
     }
-  }
 
-  fn tick(&mut self, m: usize) -> anyhow::Result<()> {
-    let mut s = b"\n  ".to_vec();
-    for t in 0..self.height {
-      for x in 0..self.width {
-        let d = (self.v)(
-          x as f64 / self.width as f64 * 3.0,
-          t as f64 / (self.height as f64 - C),
-          m as f64,
-        );
-        s.push(b" .:-=+*&#%@"[d % 11]);
-      }
-      s.extend_from_slice(b"\n  ");
+    fn tick(&mut self, m: usize) -> anyhow::Result<()> {
+        let mut s = b"\n  ".to_vec();
+        for t in 0..self.height {
+            for x in 0..self.width {
+                let d = (self.v)(
+                    x as f64 / self.width as f64 * 3.0,
+                    t as f64 / (self.height as f64 - C),
+                    m as f64,
+                );
+                s.push(b" .:-=+*&#%@"[d % 11]);
+            }
+            s.extend_from_slice(b"\n  ");
+        }
+        self.state = String::from_utf8(s)?;
+        Ok(())
     }
-    self.state = String::from_utf8(s)?;
-    Ok(())
-  }
 }
